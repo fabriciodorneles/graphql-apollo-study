5608a63ec7dbf3089c8d53f61352553c59819de5
Fabricio Dorneles <fabriciodorneles@gmail.com>
2022-11-02 10:33:50 -0300

    initial commit

---
+++ backend/src/resolvers/UserResolver.ts
@@ -0,0 +1,24 @@
+import crypto from 'crypto';
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { User } from "../models/User";
+
+@Resolver()
+export class UserResolver {
+    private data: User[] = [];
+
+    @Query(() => [User])
+    async users() {
+        return this.data;
+    }
+
+    @Mutation(() => User) 
+    async createUser(
+        @Arg('name') name: string
+    ) {        
+        const user = {id: crypto.randomUUID(), name}
+
+        this.data.push(user);
+
+        return user;
+    }
+}
